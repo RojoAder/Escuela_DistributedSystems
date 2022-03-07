ff8e292dd509d3ad900c5f9091a5f4d9523af19d
Ader <ader.rojo@toshibagcs.com>
2022-03-07 01:25:49 -0600

    CLiente

---
+++ src/main/java/classes/Person.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author aderrojo
+ */
+public class Person {
+    
+    private String name;
+    private double weight;
+    private double height;
+
+    public Person() {}
+    
+    public Person(String name, double weight, double height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", weight=" + weight + ", height=" + height + '}';
+    }
+    
+}
